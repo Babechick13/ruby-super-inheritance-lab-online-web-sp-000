18a12bad6149bdb1f3cf781b7b609b72bce84051
Jessica Natovich <jessicanatovich@yahoo.com>
2021-01-16 01:01:55 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,4 +1,5 @@
 class ChattyStudent < Student
+
   def hello
     super
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
@@ -15,9 +16,5 @@ class ChattyStudent < Student
     super
     super
     super
-    10.times do
-      super
-    end
   end
-end 
 end
